f5a2827392fbbb89bfd844473b9b8fbfc6570ff9
BaoNguyen <baokx2k3@gmail.com>
2024-03-22 21:13:20 +0700

    up

--- overview_reactjs_jsx/src/Exercise.jsx
+++ overview_reactjs_jsx/src/Exercise.jsx
@@ -48,12 +48,39 @@ function Exercise() {
 
             <div className="wrap-container">
                 <h2>[Bài tập] Tạo Element thể hiện Profile Card (như hình minh hoạ)</h2>
-                <div class="card" style={{width: "18rem"}}>
+                <div class="card" style={{ width: "18rem" }}>
                     <img src="https://admin.vov.gov.vn/UploadFolder/KhoTin/Images/UploadFolder/VOVVN/Images/sites/default/files/2024-02/elon_musk.jpg" class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="wrap-container">
+                <h2>[Bài tập] Tạo Element thể hiện Sign in Form sử dụng Bootstrap</h2>
+                <div className="container d-flex align-items-center text-center">
+                    <div className="form-signin">
+                        <form>
+                            <img className="mb-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png" alt="" width="72" height="57" />
+                            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                            <div className="form-floating">
+                                <input type="email" className="form-control email" id="floatingInput" placeholder="name@example.com" />
+                                <label>Email address</label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="password" className="form-control password" id="floatingPassword" placeholder="Password" />
+                                <label>Password</label>
+                            </div>
+                            <div className="checkbox mb-3">
+                                <label>
+                                    <input type="checkbox" /> Remember me
+                                </label>
+                            </div>
+                            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                            <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
